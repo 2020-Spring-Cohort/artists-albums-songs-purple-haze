6801c5cc810861683801dee90f73bcd7bd73ba7a
Innocent-Mbanda <mbandainnocent@gmail.com>
2020-03-18 14:07:36 -0400

    added song controller and song controller tests and they all passing

---
+++ backend-api/src/main/java/org/wcci/apimastery/models/SongController.java
@@ -0,0 +1,23 @@
+package org.wcci.apimastery.models;
+
+import javafx.print.Collation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wcci.apimastery.SongRepository;
+
+import java.util.Collection;
+
+@RestController
+public class SongController {
+    private SongRepository songRepository;
+
+    public SongController (SongRepository songRepository){
+        this.songRepository =songRepository;
+
+    }
+   @RequestMapping("songs")
+    public Collection<Song> retrievedSongs(){ return (Collection<Song>) songRepository.findAll();}
+
+   }
+
