cbb98c2ffb40295ebc1f7fa99d884311c34dd248
Hetal Nakodkar <hnakodkar@yahoo.com>
2020-03-20 15:16:49 -0400

    made a patchrequest in artist controller for album

--- backend-api/src/main/java/org/wcci/apimastery/ArtistController.java
+++ backend-api/src/main/java/org/wcci/apimastery/ArtistController.java
@@ -3,16 +3,20 @@ package org.wcci.apimastery;
 
 
 import org.springframework.web.bind.annotation.*;
+import org.wcci.apimastery.models.Album;
 import org.wcci.apimastery.models.Artist;
 
 import java.util.Collection;
+import java.util.Properties;
 
 @RestController
 public class ArtistController {
     private ArtistRepository artistRepository;
+    private AlbumRepository albumRepository;
 
-    public ArtistController(ArtistRepository artistRepository) {
+    public ArtistController(ArtistRepository artistRepository, AlbumRepository albumRepository) {
         this.artistRepository = artistRepository;
+        this.albumRepository = albumRepository;
     }
 
     @GetMapping("/artists")
@@ -21,11 +25,17 @@ public class ArtistController {
     }
 
     @GetMapping("/artists/{id}")
-    public Artist retrieveArtists(@PathVariable Long id) {
+    public Artist retrieveSingleArtist(@PathVariable Long id) {
         return artistRepository.findById(id).get();
     }
-
-    @RequestMapping("/artists")
+    @PatchMapping("/artists/{id}/albums")
+    public Artist updateArtistProducts(@PathVariable Long id, @RequestBody String requestBodyAlbum) {
+        Artist artistToPatch = artistRepository.findById(id).get();
+        Album albumToAdd = new Album(requestBodyAlbum, artistToPatch);
+        albumRepository.save(albumToAdd);
+        return artistRepository.save(artistToPatch);
+    }
+    @PostMapping("/artists")
     public Artist createArtist(@RequestBody Artist artistToAdd) {
         return artistRepository.save(artistToAdd);
     }
