e721c6dcc4cbc2642f6228773d65edd9dda1aef3
NightingaleMedia.AlbertMacbook <alsigman@gmail.com>
2020-03-25 15:32:17 -0400

    added the app.js - [x] added an app.js class - [x] started making modules instead one big class - [x] got import and export figured out

---
+++ frontend-spa/src/js/albumsBuilder.js
@@ -0,0 +1,19 @@
+export {
+    displayAllAlbums
+}
+import {
+    renderAllArtists
+} from './app.js';
+
+
+//fetch from the controller
+
+//get the json
+
+//feed the json to the for loop
+
+/// create anchor to append to
+
+const displayAllAlbums = () => {
+    return null;
+}
